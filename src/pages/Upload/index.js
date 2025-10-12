164aacde7c29e293dd1b9151e16fe89f8bf414a2
nguyenhung252006 <nguyenviethungk56@gmail.com>
2025-10-12 16:17:13 +0700

    update 1 at 12/10/2025

--- src/pages/Upload/index.js
+++ src/pages/Upload/index.js
@@ -1,6 +1,11 @@
+import Header from "~/components/Layout/DefaltLayout/Header";
 function Upload() {
     return ( 
-        <h2>Upload Page</h2>
+        <div>
+            <Header/>
+            <div className="content"></div>
+            <h2>Upload Page</h2>
+        </div>
      );
 }
 
